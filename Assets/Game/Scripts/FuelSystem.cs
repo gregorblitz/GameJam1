77b2a68b66498e079d5eaa39404b26e1a29a0559
christdanielnm <43271269+christdanielnm@users.noreply.github.com>
2026-03-10 16:41:45 -0500

    implementacion-combustible

---
+++ Assets/Game/Scripts/FuelSystem.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FuelSystem : MonoBehaviour
+{
+    public float maxFuel = 100f;
+    public float currentFuel;
+    public float fuelDrainRate = 0.5f;
+
+    public Slider fuelSlider;
+
+    void Start()
+    {
+        fuelSlider.minValue = 0;
+        fuelSlider.maxValue = maxFuel;
+        fuelSlider.value = maxFuel;
+        currentFuel = maxFuel;
+        UpdateFuelUI();
+    }
+
+    void Update()
+    {
+        currentFuel -= fuelDrainRate * Time.deltaTime;
+        currentFuel = Mathf.Clamp(currentFuel, 0f, maxFuel);
+
+        UpdateFuelUI();
+
+        if (currentFuel <= 0f)
+        {
+            Debug.Log("Se acabó el combustible");
+        }
+    }
+
+    void UpdateFuelUI()
+    {
+        fuelSlider.maxValue = maxFuel;
+        fuelSlider.value = currentFuel;
+    }
+}
